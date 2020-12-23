f010d5949e7ff898c3a60a254a25fc250812bde7
MyTh1c2L <75614334+MyTh1c2L@users.noreply.github.com>
2020-12-23 19:35:37 +0400

    Изменен шрифт

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace TechnicalSupport
+namespace TechnicalSupport
 {
     partial class MainForm
     {
@@ -56,10 +56,10 @@
             // ProblemBox
             // 
             this.ProblemBox.FormattingEnabled = true;
-            this.ProblemBox.Location = new System.Drawing.Point(9, 38);
+            this.ProblemBox.Location = new System.Drawing.Point(10, 44);
             this.ProblemBox.Margin = new System.Windows.Forms.Padding(2);
             this.ProblemBox.Name = "ProblemBox";
-            this.ProblemBox.Size = new System.Drawing.Size(223, 21);
+            this.ProblemBox.Size = new System.Drawing.Size(259, 23);
             this.ProblemBox.TabIndex = 0;
             this.ProblemBox.SelectedIndexChanged += new System.EventHandler(this.ProblemBox_SelectedIndexChanged);
             // 
@@ -67,19 +67,19 @@
             // 
             this.label1.AutoEllipsis = true;
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 23);
+            this.label1.Location = new System.Drawing.Point(10, 27);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(140, 13);
+            this.label1.Size = new System.Drawing.Size(155, 15);
             this.label1.TabIndex = 1;
             this.label1.Text = "Выберите вашу проблему:";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(6, 429);
+            this.button1.Location = new System.Drawing.Point(10, 490);
             this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(225, 22);
+            this.button1.Size = new System.Drawing.Size(262, 25);
             this.button1.TabIndex = 3;
             this.button1.Text = "Проблема не решена";
             this.button1.UseVisualStyleBackColor = true;
@@ -88,50 +88,51 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 410);
+            this.label2.Location = new System.Drawing.Point(7, 473);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(237, 13);
+            this.label2.Size = new System.Drawing.Size(261, 15);
             this.label2.TabIndex = 4;
             this.label2.Text = "Если вы не нашли решение своей проблемы:";
             // 
             // TariffBox
             // 
             this.TariffBox.FormattingEnabled = true;
-            this.TariffBox.Location = new System.Drawing.Point(9, 232);
+            this.TariffBox.Location = new System.Drawing.Point(10, 268);
             this.TariffBox.Margin = new System.Windows.Forms.Padding(2);
             this.TariffBox.Name = "TariffBox";
-            this.TariffBox.Size = new System.Drawing.Size(219, 21);
+            this.TariffBox.Size = new System.Drawing.Size(255, 23);
             this.TariffBox.TabIndex = 5;
             this.TariffBox.SelectedIndexChanged += new System.EventHandler(this.TariffBox_SelectedIndexChanged);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(505, 410);
+            this.label3.Location = new System.Drawing.Point(589, 473);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(170, 13);
+            this.label3.Size = new System.Drawing.Size(183, 15);
             this.label3.TabIndex = 6;
             this.label3.Text = "Скорость интернет соединения:";
             // 
             // Speedlabel
             // 
             this.Speedlabel.AutoSize = true;
+            this.Speedlabel.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Speedlabel.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.Speedlabel.Location = new System.Drawing.Point(505, 435);
+            this.Speedlabel.Location = new System.Drawing.Point(589, 493);
             this.Speedlabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.Speedlabel.Name = "Speedlabel";
-            this.Speedlabel.Size = new System.Drawing.Size(40, 13);
+            this.Speedlabel.Size = new System.Drawing.Size(45, 17);
             this.Speedlabel.TabIndex = 7;
             this.Speedlabel.Text = "0 Кб/с";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(9, 214);
+            this.label5.Location = new System.Drawing.Point(10, 247);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(157, 13);
+            this.label5.Size = new System.Drawing.Size(170, 15);
             this.label5.TabIndex = 9;
             this.label5.Text = "Узнайте больше про тарифы:";
             // 
@@ -179,8 +180,8 @@
             this.справкаToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
-            this.menuStrip1.Size = new System.Drawing.Size(687, 24);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(801, 24);
             this.menuStrip1.TabIndex = 10;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -189,12 +190,14 @@
             this.открытьНовуюБДToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.бДПроблемИИхРешенийToolStripMenuItem,
             this.бДТарифовToolStripMenuItem});
+            this.открытьНовуюБДToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.открытьНовуюБДToolStripMenuItem.Name = "открытьНовуюБДToolStripMenuItem";
             this.открытьНовуюБДToolStripMenuItem.Size = new System.Drawing.Size(91, 20);
             this.открытьНовуюБДToolStripMenuItem.Text = "Обновить БД";
             // 
             // бДПроблемИИхРешенийToolStripMenuItem
             // 
+            this.бДПроблемИИхРешенийToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.бДПроблемИИхРешенийToolStripMenuItem.Name = "бДПроблемИИхРешенийToolStripMenuItem";
             this.бДПроблемИИхРешенийToolStripMenuItem.Size = new System.Drawing.Size(222, 22);
             this.бДПроблемИИхРешенийToolStripMenuItem.Text = "БД проблем и их решений";
@@ -202,6 +205,7 @@
             // 
             // бДТарифовToolStripMenuItem
             // 
+            this.бДТарифовToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.бДТарифовToolStripMenuItem.Name = "бДТарифовToolStripMenuItem";
             this.бДТарифовToolStripMenuItem.Size = new System.Drawing.Size(222, 22);
             this.бДТарифовToolStripMenuItem.Text = "БД тарифов";
@@ -209,6 +213,7 @@
             // 
             // справкаToolStripMenuItem
             // 
+            this.справкаToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
             this.справкаToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
             this.справкаToolStripMenuItem.Text = "Справка";
@@ -216,24 +221,24 @@
             // 
             // ProblemText
             // 
-            this.ProblemText.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.ProblemText.Location = new System.Drawing.Point(9, 65);
+            this.ProblemText.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ProblemText.Location = new System.Drawing.Point(10, 75);
             this.ProblemText.Multiline = true;
             this.ProblemText.Name = "ProblemText";
             this.ProblemText.ReadOnly = true;
             this.ProblemText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.ProblemText.Size = new System.Drawing.Size(663, 146);
+            this.ProblemText.Size = new System.Drawing.Size(773, 168);
             this.ProblemText.TabIndex = 11;
             // 
             // TariffText
             // 
-            this.TariffText.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold);
-            this.TariffText.Location = new System.Drawing.Point(9, 259);
+            this.TariffText.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TariffText.Location = new System.Drawing.Point(10, 299);
             this.TariffText.Multiline = true;
             this.TariffText.Name = "TariffText";
             this.TariffText.ReadOnly = true;
             this.TariffText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.TariffText.Size = new System.Drawing.Size(663, 148);
+            this.TariffText.Size = new System.Drawing.Size(773, 170);
             this.TariffText.TabIndex = 12;
             // 
             // timer1
@@ -243,9 +248,9 @@
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(687, 463);
+            this.ClientSize = new System.Drawing.Size(801, 534);
             this.Controls.Add(this.TariffText);
             this.Controls.Add(this.ProblemText);
             this.Controls.Add(this.label5);
@@ -257,6 +262,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.ProblemBox);
             this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.HelpButton = true;
             this.MainMenuStrip = this.menuStrip1;
             this.Margin = new System.Windows.Forms.Padding(2);
